03b9c50d72b4d4e1741b881d5bc7f0856e5352cb
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2022-09-16 09:43:44 +0530

    payment & card details

---
+++ models/paymentModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose"); 
+
+const PaymentSchema = new mongoose.Schema({
+
+	name: {
+		type: String,
+		required: true,
+	},
+    email: {
+		type: String,
+		required: true,
+	},
+    mobile: {
+		type: String,
+		required: true,
+	},
+
+    address:{
+        type:String,
+        required:true,
+    },
+    method:{
+        type:String,
+        required:true,
+    },
+    
+},
+{
+	timestamps: true,
+}
+);
+
+module.exports = Payment = mongoose.model("Payment", PaymentSchema);
